dad28f5f951bbd4cdea58b404d1a0f033d11e1b2
jairoSenac <jairocesa@outlook.com.br>
2022-09-28 16:07:12 -0300

    Loja de roupas tela 1, Projet Uc09 atividade 1 modulo C

---
+++ App.js
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+import{Text,View,} from 'react-native';
+import  estilo from './src/estilos/estilo';
+
+Icon.loadFont();
+
+
+
+class App extends Component{
+  render(){
+    return(
+
+        <View>
+
+        </View>
+    )
+  }
+}
+
+
+
+export default App
---
+++ src/estilos/estilo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const containers =StyleSheet.create({
+    container:{backgroundColor:'black',width:50,height:50}
+})
+    
+
+
+export default containers
